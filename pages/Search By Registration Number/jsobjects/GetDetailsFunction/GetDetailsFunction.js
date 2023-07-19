2597f5344110edcfe1a900b7dba818af0e1b2ad2
Saiyam Kalra <saiyam@gmail.com>
2023-07-19 07:34:00 +0000

    Search By Registration Page Fixed

--- pages/Search By Registration Number/jsobjects/GetDetailsFunction/GetDetailsFunction.js
+++ pages/Search By Registration Number/jsobjects/GetDetailsFunction/GetDetailsFunction.js
@@ -1,25 +1,27 @@
 export default {
   async updateTextBox() {
-    try {
-      const data = await SearchByRegNumberAPI.data;
-			if (data.length>1 || data.length==0) {
-				return 'Error';
-			}
+    	try {
+				SearchByRegNumberAPI.run()
+				const data = await SearchByRegNumberAPI.data
+				if (data.length>1 || data.length==0) {
+					return 'Multiple Data or No Data Error';
+				}
       const firstResult = data[0];
       const regno = firstResult['S.NO'];
       const address = firstResult.ADDRESS;
       const name = firstResult.NAME;
       const mobile = firstResult.MOBILE;
       const amount = firstResult.AMOUNT;
-      // Text6.text = regno;
-      // Text7.text = name;
-      // Text8.text = address;
-      // Text9.text = mobile;
-      // Text10.text = amount;
-      return firstResult;
-    } catch (error) {
-      console.error('An error occurred:', error);
-      return 'Error';
-    }
+			Container.isVisible = true;
+      Text6.text = regno.toString();
+      Text7.text = name.toString();
+      Text8.text = address.toString();
+      Text9.text = mobile.toString();
+      Text10.text = amount.toString();
+      return firstResult
+    	} catch (error) {
+				console.error('An error occurred:', error);
+				return 'Error';
+    	}
   }
 }
