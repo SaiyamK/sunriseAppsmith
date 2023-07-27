3cfcd4ba42d53adb70e061047bd3696b284a688a
Saiyam Kalra <saiyam@gmail.com>
2023-07-27 20:46:55 +0000

    Error Modal Fix For Search By Registration Page

--- pages/Search By Registration/jsobjects/GetDetailsFunction/GetDetailsFunction.js
+++ pages/Search By Registration/jsobjects/GetDetailsFunction/GetDetailsFunction.js
@@ -1,11 +1,17 @@
 export default {
   async updateTextBox() {
     	try {
-				SearchByRegNumberAPI.run()
 				const data = await SearchByRegNumberAPI.data
-				if (data.length>1 || data.length==0) {
-					Text12.text = 'Multiple Data or No Data Error'.toString();
-					return 'Multiple Data or No Data Error';
+				if (data.length==0) {
+					Text12.text = 'No Data Found!'.toString();
+					showModal('ErrorModal');
+					return 'No Data Found!';
+				}
+				if (data.length>1) {
+					Text12.text = 'Multiple Records Found!'.toString();
+					ErrorModal.isVisible = true;
+					showModal('ErrorModal');
+					return 'Multiple Records Found!';
 				}
       const firstResult = data[0];
       const regno = firstResult['S.NO'];
@@ -21,8 +27,8 @@ export default {
       Text10.text = amount.toString();
       return firstResult
     	} catch (error) {
-				console.error('An error occurred:', error);
 				Text12.text = 'API Error'.toString();
+				showModal('ErrorModal');
 				return 'API Error';
     	}
   }
